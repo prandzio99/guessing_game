f52a4271bbb39850d991f6c85af4692ebabd7d63
Paweł Randzio <pawelrandzio99@gmail.com>
2022-05-12 08:16:40 +0200

    Added omitted setup script :)
    Signed-off-by: Paweł Randzio <pawelrandzio99@gmail.com>

---
+++ setup.sh
@@ -0,0 +1,32 @@
+echo -en "Checking if ruby is installed... "
+
+GREEN='\033[0;32m'
+YELLOW='\033'
+END='\033[0m'
+
+if [ -f "/usr/bin/ruby" ]; then
+    echo -en "${GREEN}OK.${END}\n"
+else
+    echo -en "${YELLOW}installing...${END}\n"
+    sudo apt-get --assume-yes install ruby
+fi
+echo -en "Installed: "
+ruby --version
+
+echo -en "Checking if required gems installed...\n"
+echo -en "Colorize... "
+if [ $(gem list -i "^colorize$") == "true" ]; then
+    echo -en "${GREEN}OK.${END}\n"
+else
+    echo -en "${YELLOW}installing...${END}\n"
+    sudo gem install colorize
+fi
+echo -en "ParseConfig... "
+if [ $(gem list -i "^parseconfig$") == "true" ]; then
+    echo -en "${GREEN}OK.${END}\n"
+else
+    echo -en "${YELLOW}installing...${END}\n"
+    sudo gem install parseconfig
+fi
+
+echo -en "${GREEN}Setup complete!${END}\n"
